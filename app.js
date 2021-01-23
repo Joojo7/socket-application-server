cfb0c947e55c3d4af309822c3afc70b4d65b663b
Joojo Dontoh <joojodontoh@gmail.com>
2021-01-23 23:17:25 +0800

    new files

---
+++ app.js
@@ -0,0 +1,42 @@
+const httpServer = require('http').createServer((req, res) => {
+
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  });
+
+
+  const io = require('socket.io')(httpServer, {
+    cors: {
+      origin: "http://localhost:8000",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true
+    }
+  });
+
+  const users = {}
+  
+  io.on('connection', socket => {
+      socket.on('send-chat-message', msg => {
+        socket.broadcast.emit('chat-message', {message: msg, name: users[socket.id]})
+    })
+
+    socket.on('username', username => {
+        users[socket.id] = username
+        socket.broadcast.emit('user-in', username)
+    })
+
+    socket.on('disconnect', () => {
+      socket.broadcast.emit('user-disconnected', users[socket.id])
+      delete users[socket.id]
+  })
+
+     
+  });
+  
+  const PORT = process.env.PORT || 3000;
+  httpServer.listen(PORT, () => console.log(`Running server on ${process.env.ENV} 🚀. \nListening on ${ PORT } 👂`));
+
+  
